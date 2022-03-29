e5ad5abdfd99da5131cbc2675c79f02e9f401fe5
ucndaniel <57615946+ucndaniel@users.noreply.github.com>
2022-03-29 11:29:15 +0200

    navbar

---
+++ assets/script/navbar.js
@@ -0,0 +1,8 @@
+function navbar() {
+    let navbar = document.getElementById("navbar");
+    if (navbar.style.display === "grid") {
+    navbar.style.display = "none";
+        } else {
+    navbar.style.display = "grid";
+        }
+      }
--- assets/script/toggle-navbar.js
+++ assets/script/toggle-navbar.js
@@ -6,3 +6,15 @@ function classToggle() {
 
 document.querySelector('.Navbar__Link-toggle')
   .addEventListener('click', classToggle);
+
+
+
+
+  function navbar() {
+    let navbar = document.getElementById("navbar");
+    if (navbar.style.display === "grid") {
+    navbar.style.display = "none";
+        } else {
+    navbar.style.display = "grid";
+        }
+      }
